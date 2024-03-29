78004e40522a56f618c3b4302fd69c8f99248176
TumeloLutaka <tumelolutakacode@gmail.com>
2024-03-30 01:47:33 +0200

    editing server

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ import axios from "axios"
 import pg from "pg"
 import bodyParser from "body-parser"
 import "dotenv/config"
-
-const db = new pg.Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.HOST_NAME,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.PORT_NUM
-})
-
+ 
+const { Pool } = pg;
+ 
+const app = express();
+const PORT = 3000;
+ 
+const connectionString = "postgres://tumelo:hmxD4nJFBrIoRRthoDyp9LRtwf3sG9sN@dpg-co3kue21hbls73bjpntg-a.oregon-postgres.render.com/book_notes_jhsk";//you can create your postgreSQL server on render.com or Vercel and then they'll give u external URL copy that and paste it here
+ 
+const db = new Pool({
+  connectionString: connectionString,
+  // If you're using a service like Heroku, you might need this for SSL:
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 db.connect();
 
-const app =  express()
-const PORT = 3000
+// const db = new pg.Client({
+//     user: process.env.POSTGRES_USER,
+//     host: process.env.HOST_NAME,
+//     database: process.env.DATABASE_NAME,
+//     password: process.env.DATABASE_PASSWORD,
+//     port: process.env.PORT_NUM
+// })
 
 // Installing middleware
 app.use(bodyParser.urlencoded({ extended: true }));
